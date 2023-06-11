341014989bbb037fef848d8e412fbd09f9b88f3c
Jonas Merkle [JJM] <jonas@jjm.one>
2023-06-11 19:55:16 +0200

    adds more simple tests

--- src/jjm.one.RabbitMqClientWrapper.Tests/types/MessageTests.cs
+++ src/jjm.one.RabbitMqClientWrapper.Tests/types/MessageTests.cs
@@ -78,5 +78,109 @@ public class MessageTests
 
     #endregion
 
+    #region public members tests
+
+    [Fact]
+    public void MessageTest_DeliveryTagGetTest()
+    {
+        // arrange
+        ReadOnlyMemory<byte> b = new ReadOnlyMemory<byte>();
+        Message m = new Message(
+            new BasicGetResult(42, true, "TEST-EX","TEST-RK", 69,
+                null, b));
+        ulong res = 0;
+        
+        try
+        {
+            // act
+            res = m.DeliveryTag;
+        }
+        catch (Exception exc)
+        {
+            // assert 1
+            Assert.Fail(exc.Message);
+        }
+            
+        // assert 2
+        res.Should().Be(42);
+    }
+    
+    [Fact]
+    public void MessageTest_RoutingKeyGetTest()
+    {
+        // arrange
+        ReadOnlyMemory<byte> b = new ReadOnlyMemory<byte>();
+        Message m = new Message(
+            new BasicGetResult(42, true, "TEST-EX","TEST-RK", 69,
+                null, b));
+        string res = string.Empty;
+        
+        try
+        {
+            // act
+            res = m.RoutingKey;
+        }
+        catch (Exception exc)
+        {
+            // assert 1
+            Assert.Fail(exc.Message);
+        }
+            
+        // assert 2
+        res.Should().Be("TEST-RK");
+    }
+    
+    [Fact(Skip = "Not properly implemented Test.")]
+    public void MessageTest_BasicPropertiesGetTest()
+    {
+        // arrange
+        ReadOnlyMemory<byte> b = new ReadOnlyMemory<byte>();
+        Message m = new Message(
+            new BasicGetResult(42, true, "TEST-EX","TEST-RK", 69,
+                null, b));
+        IBasicProperties? res = null!;
+        
+        try
+        {
+            // act
+            res = m.BasicProperties;
+        }
+        catch (Exception exc)
+        {
+            // assert 1
+            Assert.Fail(exc.Message);
+        }
+            
+        // assert 2
+        res.Should().BeNull();
+    }
+    
+    [Fact]
+    public void MessageTest_BodyGetTest()
+    {
+        // arrange
+        ReadOnlyMemory<byte> b = new ReadOnlyMemory<byte>();
+        Message m = new Message(
+            new BasicGetResult(42, true, "TEST-EX","TEST-RK", 69,
+                null, b));
+        ReadOnlyMemory<byte>? res = null;
+        
+        try
+        {
+            // act
+            res = m.Body;
+        }
+        catch (Exception exc)
+        {
+            // assert 1
+            Assert.Fail(exc.Message);
+        }
+            
+        // assert 2
+        res.Should().Be(b);
+    }
+
+    #endregion
+    
     #endregion
 }
