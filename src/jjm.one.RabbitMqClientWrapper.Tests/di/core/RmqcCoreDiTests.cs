f392920c73ed4216758908ab574b192b3336951f
Jonas Merkle [JJM] <jonas@jjm.one>
2023-06-12 00:36:17 +0200

    di tests

---
+++ src/jjm.one.RabbitMqClientWrapper.Tests/di/core/RmqcCoreDiTests.cs
@@ -0,0 +1,51 @@
+using FluentAssertions;
+using jjm.one.RabbitMqClientWrapper.di.core;
+using jjm.one.RabbitMqClientWrapper.main.core;
+using jjm.one.RabbitMqClientWrapper.types;
+using jjm.one.RabbitMqClientWrapper.types.di;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace jjm.one.RabbitMqClientWrapper.Tests.di.core;
+
+/// <summary>
+/// This class contains the unit tests for the <see cref="RmqcCoreDi"/> class.
+/// </summary>
+public class RmqcCoreDiTests
+{
+    #region private members
+
+    private readonly IHostBuilder _hostBuilder;
+
+    #endregion
+    
+    #region ctor
+
+    /// <summary>
+    /// The default constructor of the <see cref="RmqcCoreDiTests"/> class.
+    /// </summary>
+    public RmqcCoreDiTests()
+    {
+        _hostBuilder = Host.CreateDefaultBuilder();
+    }
+
+    #endregion
+    
+    #region tests
+
+    /// <summary>
+    /// Tests the static AddRmqcCore function.
+    /// </summary>
+    [Fact]
+    public void RmqcCoreDiTest_AddRmqcCoreTest()
+    {
+        // arrange + act
+        _hostBuilder.ConfigureServices(services =>
+            services.AddRmqcCore(new Settings(), new DiSimpleTypeWrappersEnableCoreLogging()));
+        var host = _hostBuilder.Build();
+
+        host.Services.GetService<IRmqcCore>().Should().NotBeNull();
+    }
+
+    #endregion
+}
