81890cad5eb6f7a554595ea1283a22b970d3f50c
Jonas Merkle [JJM] <jonas@jjm.one>
2023-06-12 14:16:06 +0200

    fixes code smell from sonarqube
    https://sonarcloud.io/project/issues?open=AYivKBnETXlHjJ1VIwwl&id=jjm-one_jjm.one.RabbitMqClientWrapper

--- src/jjm.one.RabbitMqClientWrapper/types/exceptions/UnInitializedClientException.cs
+++ src/jjm.one.RabbitMqClientWrapper/types/exceptions/UnInitializedClientException.cs
@@ -10,12 +10,21 @@ namespace jjm.one.RabbitMqClientWrapper.types.exceptions;
 [Serializable]
 public class UnInitializedClientException : Exception
 {
+    #region private members
+
+    private string? _operation = string.Empty;
+
+    #endregion
+    
     #region public members
 
     /// <summary>
     /// The name of the operation which causes the exception.
     /// </summary>
-    public string? Operation = string.Empty;
+    public string? Operation {
+        get => _operation;
+        set => _operation = value;
+    }
 
     #endregion
     
