9b98b1acf363f0adc316e8dde76f1efb4f9a97f9
Jonas Merkle [JJM] <jonas@jjm.one>
2023-08-29 14:16:02 +0200

    updates time span tools

--- src/jjm.one.RabbitMqClientWrapper/util/TimeSpanTools.cs
+++ src/jjm.one.RabbitMqClientWrapper/util/TimeSpanTools.cs
@@ -20,13 +20,13 @@ internal static class TimeSpanTools
         var temp = milliseconds;
         
         var d = temp / 86400000;
-        temp = temp % 86400000;
+        temp %= 86400000;
         var h = temp / 3600000;
-        temp = temp % 3600000;
+        temp %= 3600000;
         var m = temp / 60000;
-        temp = temp % 60000;
+        temp %= 60000;
         var s = temp / 1000;
-        temp = temp % 1000;
+        temp %= 1000;
 
         return new TimeSpan(days: d, hours: h, minutes: m, seconds: s, milliseconds: temp);
     }
